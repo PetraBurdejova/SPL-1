af3847f60737e19bfe0cc38c5db37b404a87977a
Matthias2193 <matthias.becher2193@gmail.com>
2018-05-28 15:15:35 +0200

    Removed redundant code from DataPreparation

--- DataPreparation.R
+++ DataPreparation.R
@@ -54,22 +54,3 @@ pcaFunc3 <- function(data){
 facFunc <- function(data){
   return(fa(data[,8:57],nfactors = 5))
 }
-
-# avgAge1 <- apply(fiveFactors[fiveFactors$ageCat == 1,9:13],2,mean,na.rm=T)
-# avgAge2 <- apply(fiveFactors[fiveFactors$ageCat == 2,9:13],2,mean,na.rm=T)
-# avgAge3 <- apply(fiveFactors[fiveFactors$ageCat == 3,9:13],2,mean,na.rm=T)
-# avgAge4 <- apply(fiveFactors[fiveFactors$ageCat == 4,9:13],2,mean,na.rm=T)
-# avgAge5 <- apply(fiveFactors[fiveFactors$ageCat == 5,9:13],2,mean,na.rm=T)
-# avgAge6 <- apply(fiveFactors[fiveFactors$ageCat == 6,9:13],2,mean,na.rm=T)
-# avgAge7 <- apply(fiveFactors[fiveFactors$ageCat == 7,9:13],2,mean,na.rm=T)
-# avgAge8 <- apply(fiveFactors[fiveFactors$ageCat == 8,9:13],2,mean,na.rm=T)
-# avgAge9 <- apply(fiveFactors[fiveFactors$ageCat == 9,9:13],2,mean,na.rm=T)
-# 
-# ages <- data.frame(rbind(avgAge1,avgAge2,avgAge3,avgAge4,avgAge5,avgAge6,avgAge7,avgAge8,avgAge9))
-# 
-# plot(c(1:9),ages$Intro.Extra,type = "l",col="blue",ylim = c(-1.8,1.8),xlab = "Age Category", ylab = "Values of Traits", main = "Average values of the five traits in different age groups")
-# lines(ages$Neuro,col="red")
-# lines(ages$Agree,col="green")
-# lines(ages$Openess,col="orange")     
-# lines(ages$Conscient,col="black")
-#legend("topleft", inset=.05, title="Trait", c("Extra","Neuro","Agree","Openess","Conscient"), fill=c("blue","red","green","orange","black"), horiz=TRUE,)
