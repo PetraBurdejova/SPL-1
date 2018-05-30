902240f540a9219fa130610f68b96d46783832ba
sahnouna <asomilan0205@gmail.com>
2018-05-30 14:26:21 +0200

    average between males and females

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,3 +1,7 @@
 source("DataPreparation.R")
 fiveFactors <- facFunc(data)
 
+males = fiveFactors[fiveFactors$gender==1,]
+females =fiveFactors[fiveFactors$gender==2,]
+males[,9]
+mean(males[,9])-mean(females[,9])
