7d1d9d38fb629cb5a8217661361ee437c13e6cda
Matthias2193 <matthias.becher2193@gmail.com>
2018-07-30 18:18:39 +0200

    Updated file, so it can be run without previously running "Test.R"

--- DataAnalysis_PTGenderAgeGroup.R
+++ DataAnalysis_PTGenderAgeGroup.R
@@ -1,5 +1,22 @@
 #Tests for defining the Average values of all personality traits between males and females over the age
 
+source("DataPreparation.R")
+
+fiveFactors = getDataSetWithBig5(data, FALSE,T)
+
+#All People
+avgAge1 <- apply(fiveFactors[fiveFactors$ageCat == 1,9:13],2,mean,na.rm=T)
+avgAge2 <- apply(fiveFactors[fiveFactors$ageCat == 2,9:13],2,mean,na.rm=T)
+avgAge3 <- apply(fiveFactors[fiveFactors$ageCat == 3,9:13],2,mean,na.rm=T)
+avgAge4 <- apply(fiveFactors[fiveFactors$ageCat == 4,9:13],2,mean,na.rm=T)
+avgAge5 <- apply(fiveFactors[fiveFactors$ageCat == 5,9:13],2,mean,na.rm=T)
+avgAge6 <- apply(fiveFactors[fiveFactors$ageCat == 6,9:13],2,mean,na.rm=T)
+avgAge7 <- apply(fiveFactors[fiveFactors$ageCat == 7,9:13],2,mean,na.rm=T)
+avgAge8 <- apply(fiveFactors[fiveFactors$ageCat == 8,9:13],2,mean,na.rm=T)
+avgAge9 <- apply(fiveFactors[fiveFactors$ageCat == 9,9:13],2,mean,na.rm=T)
+
+ages <- data.frame(rbind(avgAge1,avgAge2,avgAge3,avgAge4,avgAge5,avgAge6,avgAge7))
+
 
 #Males
 mavgAge1 <- apply(fiveFactors[fiveFactors$ageCat == 1 & fiveFactors$gender==1,9:13],2,mean,na.rm=T)
@@ -33,11 +50,11 @@ fages <- data.frame(rbind(favgAge1,favgAge2,favgAge3,favgAge4,favgAge5,favgAge6,
 
 #Extraversion
 
-plot(c(1:7),mages$Intro.Extra,type = "l",col="red",ylim = c(-0.5,1.0)
+plot(c(1:7),mages$Intro,type = "l",col="red",ylim = c(-0.5,1.0)
      ,xlab = "Age Category", ylab = "Values of Traits", 
      main = "Average values of Extraversion between males and females over the age", cex.main=0.9)
-lines(fages$Intro.Extra,col="blue")
-lines(ages$Intro.Extra,col="grey")
+lines(fages$Intro,col="blue")
+lines(ages$Intro,col="grey")
 legend("topleft", inset=.05, c("Male","Female","All"), fill=c("red","blue","grey"), horiz=TRUE)
 
 #openness
@@ -84,3 +101,4 @@ plot(c(1:7),mages$Neuro,type = "l",col="red",ylim = c(-3.0,2.0)
 lines(fages$Neuro,col="blue")
 lines(ages$Neuro,col="grey")
 legend("topleft", inset=.05, c("Male","Female","All"), fill=c("red","blue","grey"), horiz=TRUE)
+
