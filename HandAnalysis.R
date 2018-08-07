fbcf20ed2aa0bc1dc52b7953d1703d122778efa0
denizgibi <guelesed@hu-berlin.de>
2018-08-07 17:43:41 +0200

    small adjustments in HandAnalysis plotting"

--- HandAnalysis.R
+++ HandAnalysis.R
@@ -7,7 +7,7 @@ library("compare")
 if (!require("tidyverse")) install.packages("tidyverse")
 library("tidyverse")
 
-df = getDataSetWithBig5(data, FALSE,F)
+df = getDataSetWithBig5(data, FALSE, FALSE)
 df$ageCat = as.factor(df$ageCat)
 
 cols = c("Intro", "Neuro", "Agree", 
@@ -22,20 +22,20 @@ df = df[(df$hand==1 | df$hand==2) & (df$ageCat!=9 & df$ageCat!=8), ]
 # Averages Big5 Right-handed
 right = df %>% 
   filter(hand == 1) %>%
-  select(cols) %>%
+  dplyr::select(cols) %>%
   group_by(ageCat) %>% 
   summarise_all(mean)
 
 # Averages Big5 Left-handed
 left = df %>% 
   filter(hand == 2) %>%
-  select(cols) %>%
+  dplyr::select(cols) %>%
   group_by(ageCat) %>% 
   summarise_all(mean)
 
 # All averages
 all = df %>% 
-  select(cols) %>%
+  dplyr::select(cols) %>%
   group_by(ageCat) %>% 
   summarise_all(mean)
 
@@ -43,7 +43,7 @@ all = df %>%
 # Extraversion
 ggplot(df, aes(x=Intro, color=hand)) +
   geom_density() + theme_light() +
-  xlim(0, 55) +
+  xlim(0, 60) +
   labs(title="Extraversion density curve", x="Intro", y="Density")
 
 ggplot(data = right, aes(x = ageCat, y=Intro)) +
@@ -60,11 +60,27 @@ ggplot(df, aes(x=Openess, color=hand)) +
   xlim(0, 60) +
   labs(title="Openness density curve", x="Openness", y="Density")
 
+ggplot(data = right, aes(x = ageCat, y=Openess)) +
+  geom_line(data = right, aes(x = ageCat, y=Openess, group = 1, color='Right-handed')) +
+  geom_line(data = left, aes(x = ageCat, y=Openess, group = 1, color='Left-handed')) + 
+  geom_line(data = all, aes(x = ageCat, y=Openess, group = 1, color='All-handed')) +
+  labs(title="Average values of Openness between left and right-handed over the age",
+       x="Age Category", y="Values of Traits") +
+  theme_light()
+
 # Conscientiousness
 ggplot(df, aes(x=Conscient, color=hand)) +
   geom_density() + theme_light() +
   xlim(0, 60) +
   labs(title="Conscientiousness density curve", x="Conscientiousness", y="Density")
+
+ggplot(data = right, aes(x = ageCat, y=Conscient)) +
+  geom_line(data = right, aes(x = ageCat, y=Conscient, group = 1, color='Right-handed')) +
+  geom_line(data = left, aes(x = ageCat, y=Conscient, group = 1, color='Left-handed')) + 
+  geom_line(data = all, aes(x = ageCat, y=Conscient, group = 1, color='All-handed')) +
+  labs(title="Average values of Conscientiousness between left and right-handed over the age",
+       x="Age Category", y="Values of Traits") +
+  theme_light()
 
 # Agreeableness
 ggplot(df, aes(x=Agree, color=hand)) +
@@ -72,8 +88,24 @@ ggplot(df, aes(x=Agree, color=hand)) +
   xlim(0, 60) +
   labs(title="Agreeableness density curve", x="Agreeableness", y="Density")
 
+ggplot(data = right, aes(x = ageCat, y=Agree)) +
+  geom_line(data = right, aes(x = ageCat, y=Agree, group = 1, color='Right-handed')) +
+  geom_line(data = left, aes(x = ageCat, y=Agree, group = 1, color='Left-handed')) + 
+  geom_line(data = all, aes(x = ageCat, y=Agree, group = 1, color='All-handed')) +
+  labs(title="Average values of Agreeableness between left and right-handed over the age",
+       x="Age Category", y="Values of Traits") +
+  theme_light()
+
 # Neuroticism
 ggplot(df, aes(x=Neuro, color=hand)) +
   geom_density() + theme_light() +
   xlim(0, 60) +
   labs(title="Neuroticism density curve", x="Neuroticism", y="Density")
+
+ggplot(data = right, aes(x = ageCat, y=Neuro)) +
+  geom_line(data = right, aes(x = ageCat, y=Neuro, group = 1, color='Right-handed')) +
+  geom_line(data = left, aes(x = ageCat, y=Neuro, group = 1, color='Left-handed')) + 
+  geom_line(data = all, aes(x = ageCat, y=Neuro, group = 1, color='All-handed')) +
+  labs(title="Average values of Neuroticism between left and right-handed over the age",
+       x="Age Category", y="Values of Traits") +
+  theme_light()
