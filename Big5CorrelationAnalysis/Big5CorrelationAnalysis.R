ce1005e2e9ee2d9c0a5f4306ac6bcaa4788a9eba
Matthias2193 <matthias.becher2193@gmail.com>
2018-08-09 11:21:50 +0200

    Formatting

--- Big5CorrelationAnalysis/Big5CorrelationAnalysis.R
+++ Big5CorrelationAnalysis/Big5CorrelationAnalysis.R
@@ -3,16 +3,16 @@ if (!require("corrplot")) install.packages("corrplot")
 library(corrplot)
 
 
-big5 = getCombinedData(data,F)
+big5 = getCombinedData(data, F)
 traits = c("Intro", "Neuro", "Openess", "Conscient", "Agree")
 
-big5Cor = cor(big5[,traits])
+big5Cor = cor(big5[, traits])
 big5Cor
-corrplot.mixed(big5Cor,lower.col = "black", upper ="square")
+corrplot.mixed(big5Cor, lower.col = "black", upper = "square")
 
 
 big5$gender = as.numeric(levels(big5$gender))[big5$gender]
-corWithAgeGender = cor(big5[,c(traits,c("age","gender"))])
-corrplot.mixed(corWithAgeGender,lower.col = "black", upper ="square")
-corrplot.mixed(corWithAgeGender,lower.col = "black", upper ="square",order="AOE")
+corWithAgeGender = cor(big5[, c(traits, c("age", "gender"))])
+corrplot.mixed(corWithAgeGender, lower.col = "black", upper = "square")
+corrplot.mixed(corWithAgeGender, lower.col = "black", upper = "square", order = "AOE")
 
